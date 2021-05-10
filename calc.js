b1932e3748e3ff6273b4c1a27e49658dd1fa11a3
Stephen Wuh <swuh87@gmail.com>
2021-05-10 19:38:57 -0400

    file updated

--- calc.js
+++ calc.js
@@ -13,9 +13,30 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
 
+		if (mathSymbol === "+") {
+			console.log(num1 + num2);
+		} 
+		if (mathSymbol === "-") {
+			console.log(num1-num2);
+		} 
+		if (mathSymbol === "*") {
+			console.log(num1*num2);
+		}
+		if (mathSymbol === "/") {
+			console.log(num1/num2);
+		} 
+		if (mathSymbol === "sqrt") {
+			console.log(Math.pow(num1, 1/num2));
+		}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
+
+
